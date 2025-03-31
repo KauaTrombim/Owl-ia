b71332d22a135a77c377444aa2eecae781225491
kaua <kauabenjamint@gmail.com>
2025-03-30 22:41:26 -0300

    Sistema de Cadastro

--- api/DBconfig.js
+++ api/DBconfig.js
@@ -3,12 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebas
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyALLoWQnBTgVC0hQON2jkXw83T4q9kGv54",
-  authDomain: "owl-ia.firebaseapp.com",
-  projectId: "owl-ia",
-  storageBucket: "owl-ia.firebasestorage.app",
-  messagingSenderId: "477853252781",
-  appId: "1:477853252781:web:313f4ffe0c226c0905da24"
+
 };
 
 // Initialize Firebase
